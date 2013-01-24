5c41fcf3dd70d17487478bd611c2fb02e8b5a2dd
ashikajith <ashik@rubykitchen.org>
2013-01-24 11:56:42 +0530

    The printing methods begining level testing

---
+++ all_sorts_of_printing_introduction.rb
@@ -0,0 +1,19 @@
+#simple printing 
+print 1..5
+for ss in 1...5
+	puts 'Hello'
+end
+string ="My name is Ashik\n"
+string.split('').each do |c| 
+	puts c
+end
+
+#array
+array=[1,2,3,4,5,6]
+array_words=["manu",'shyam',"papu",'lalu']
+p  array.index(4)
+
+p array_words[3]
+p array_words[0]
+p array_words.[](1)
+
